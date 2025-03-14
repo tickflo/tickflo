a24cb53836ef35c9a7ca0b96bfbf047e62db7a7c
Richard Leek <richard@richardleek.com>
2025-03-14 17:51:00 +0100

    Make reset db run setup

--- app/.server/db/reset.ts
+++ app/.server/db/reset.ts
@@ -1,13 +1,11 @@
 import { sql } from 'drizzle-orm';
+import { setupDb } from 'server/setup-db';
 import { db } from '~/.server/db';
-import { applyMigrations } from './migrate';
-import { seed } from './seed';
 
 export async function reset() {
   await db.execute(sql`drop schema if exists public cascade`);
   await db.execute(sql`create schema public`);
   await db.execute(sql`drop schema if exists drizzle cascade`);
 
-  await applyMigrations();
-  await seed();
+  await setupDb();
 }
